b4e55bbe52f960a299e7874e6d174086bb393efc
Fernando Goncalves <fernando.hmg@gmail.com>
2021-05-10 23:01:49 -0300

    Creating global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <GlobalStyle />
     </div>
   );
 }
