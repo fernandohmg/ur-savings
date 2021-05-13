38d524fe06325c8d0d62c79465f891a69ef31e13
Fernando Goncalves <fernando.hmg@gmail.com>
2021-05-12 22:55:11 -0300

    modal style and form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from "react-modal";
+import closeImg from "../../assets/close.svg";
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +12,23 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Save transaction</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button onClick={onRequestClose} className="react-modal-close">
+        <img src={closeImg} alt="Close modal" />
+      </button>
+      <Container>
+        <h2>Save transaction</h2>
+        <input type="text" placeholder="Title" />
+        <input type="number" placeholder="Value" />
+        <input type="text" placeholder="Category" />
+
+        <button type="submit">Save</button>
+      </Container>
     </Modal>
   );
 }
